27ca24fe6e3fb3381e36da24a4a3e3deb623db52
Seonggoo Kang <ksung91889@naver.com>
2023-06-21 03:33:45 +0900

    빈 배열 input 유효성 검증

--- src/components/SimpleInput.js
+++ src/components/SimpleInput.js
@@ -4,6 +4,7 @@ const SimpleInput = (props) => {
   // ref를 사용해서 필요할 때 input요소로부터 값을 읽는다.
   const nameInputRef = useRef();
   const [enteredName, setEnteredName] = useState("");
+  const [enteredNameIsValid, setEnteredNameIsValid] = useState(true);
 
   // 1. onChange로 input값을 넣는 방법
   const nameInputChangeHandler = (event) => {
@@ -15,6 +16,14 @@ const SimpleInput = (props) => {
     // 웹사이트를 제공하는 서버로 HTTP 요청을 보내게 됨. 이게 자동으로 이뤄지는데 여기서 문제는
     // 실제로 우리에게는 요청을 처리할 서버가 없고 html과 자바스크립트만 전송하는  정적서버만 있다는 점.
     // 따라서 이 요청을 보내지 않도록 하기 위함.
+
+    if (enteredName.trim() === "") {
+      setEnteredNameIsValid(false);
+      // 처음과 끝의 공백을 없애줌
+      return; // 이 함수의 다음코드가 더이상 실행 되지 않게함.
+    }
+
+    setEnteredNameIsValid(true);
 
     console.log("enteredName>> ", enteredName); // state로부터 값 읽기
     // ref는 항상 current 프로퍼티를 갖는 객체.
@@ -33,9 +42,14 @@ const SimpleInput = (props) => {
 
   // 반면 즉각적인 유효성 검증을 위해  키 입력마다 입력 값이 필요하다면 >> state
   // state의 다른 장점 : 입력된 값을 초기화하고 싶을 때. (ref로도 구현 가능 but 복잡 and 지양)
+
+  const nameInputClasses = enteredNameIsValid
+    ? "form-control"
+    : "form-control invalid";
+
   return (
     <form onSubmit={formSubmissionHandler}>
-      <div className="form-control">
+      <div className={nameInputClasses}>
         <label htmlFor="name">Your Name</label>
         {/* 함수를 input 요소의 이벤트 변경에 연결하고나면 자동적으로 이벤트를 설명하는 event객체가 얻어지며 이를 통해 입력된 값을 얻을 수 있음. */}
         <input
@@ -45,6 +59,9 @@ const SimpleInput = (props) => {
           onChange={nameInputChangeHandler}
           value={enteredName}
         />
+        {!enteredNameIsValid && (
+          <p className="error-text">Name must not be empty</p>
+        )}
       </div>
       <div className="form-actions">
         <button>Submit</button>
