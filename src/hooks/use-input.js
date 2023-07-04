87b7b383a1edecfc3836981a7bdb769d17c555c7
Seonggoo Kang <ksung91889@naver.com>
2023-07-04 17:16:26 +0900

    useReducer

--- src/hooks/use-input.js
+++ src/hooks/use-input.js
@@ -1,29 +1,58 @@
-// 커스텀 훅
+// useReducer
 
-import { useState } from "react";
+import { useReducer } from "react";
+
+const initialInputState = {
+  value: "",
+  isTouched: false,
+};
+
+const inputStateReducer = (state, action) => {
+  if (action.type === "INPUT") {
+    return { value: action.value, isTouched: state.isTouched }; // isTouched를 true로 바꾸면 안된다. key stroke를 아직 마친게 아니기 때문에. so we need to use previous state
+  }
+  if (action.type === "BLUR") {
+    return { isTouched: true, value: state.value }; // 사용자가 입력칸을 건드렸다는 의미이므로 true로.value와는 관련 없으므로 value는 existging value로.
+  }
+  if (action.type === "RESET") {
+    return {
+      isTouched: false,
+      value: "",
+    };
+  }
+  return inputStateReducer;
+};
 
 const useInput = (validateValue) => {
-  const [enteredValue, setEnteredValue] = useState("");
-  const [isTouched, setIsTouched] = useState(false);
+  const [inputState, dispatch] = useReducer(
+    inputStateReducer,
+    initialInputState
+  );
 
-  const valueIsValid = validateValue(enteredValue);
-  const hasError = !valueIsValid && isTouched;
+  // const [enteredValue, setEnteredValue] = useState("");
+  // const [isTouched, setIsTouched] = useState(false);
+
+  const valueIsValid = validateValue(inputState.value);
+  const hasError = !valueIsValid && inputState.isTouched;
 
   const valueChangeHandler = (event) => {
-    setEnteredValue(event.target.value);
+    dispatch({ type: "INPUT", value: event.target.value });
+    // setEnteredValue(event.target.value);
   };
 
   const inputBlurHandler = (event) => {
-    setIsTouched(true);
+    dispatch({ type: "BLUR" });
+    // setIsTouched(true);
   };
 
   const reset = () => {
-    setEnteredValue("");
-    setIsTouched(false);
+    // setEnteredValue("");
+    // setIsTouched(false);
+    dispatch({ type: "RESET" });
   };
 
   return {
-    value: enteredValue, // enteredValue를 value로 설정
+    value: inputState.value, // enteredValue를 value로 설정
     isValid: valueIsValid,
     hasError, // value를 안 넣어주면 hasError: hasError << 이거랑 똑같음
     valueChangeHandler,
